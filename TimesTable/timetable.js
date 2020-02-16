96abf7933bd3e43c3647ac572357f58828947c00
forest1030 <surim.sohn@gmail.com>
2020-02-16 18:00:41 +0900

    use while

--- TimesTable/timetable.js
+++ TimesTable/timetable.js
@@ -1,21 +1,33 @@
 const timeTableContainer = document.querySelector(".timeTableContainer"), 
     quest = timeTableContainer.querySelector('.js-quest');
 const input = document.querySelector('input');
-const randomNum = Math.floor(Math.random() * 10)+1;
 
 
-function question(){
-    const randomA = Math.floor(Math.random() * 10)+1;
-    const randomB = Math.floor(Math.random() * 10)+1;
+function result(){
+    while(true){
+        const randomA = Math.floor(Math.random() * 10)+1;   // 1 ~ 10 random num
+        const randomB = Math.floor(Math.random() * 10)+1;   // 1 ~ 10 random num
+        const comResult = randomA * randomB;   // computer answer
+        const userResult = input.value;
+        quest.innerText = `${randomA} x ${randomB} = ?`;
+        while(true){
+            // quest.innerText = `${randomA} x ${randomB} = ?`;
+            if (userResult === comResult){
+                alert('Next question');
+            }
+            else if(userResult !== comResult){
+                alert('Do it again');
+            }
+        }
 
-    // const comAnswer = randomA * randomB;
-
-    quest.innerText = `${randomA} x ${randomB} = ?`;
+    }
+    // quest.innerText = `${randomA} x ${randomB} = ?`;
 
 }
 
+
 function init(){
-    question();
+    result();
 }
 
 init();
